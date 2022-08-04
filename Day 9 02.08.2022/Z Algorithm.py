5f2713577d1402b35ec2fba34c9db753ae3942fc
kishorekumarcodingmart <kishorekumar@codingmart.com>
2022-08-04 10:07:14 +0530

    Z Algorithm

--- Day 9 02.08.2022/Z Algorithm.py
+++ Day 9 02.08.2022/Z Algorithm.py
@@ -1,16 +1,38 @@
-txt = "abcdabcaaabc"
+def getZarr(string, z):
+	n = len(string)
+	l, r, k = 0, 0, 0
+	for i in range(1, n):
+		if i > r:
+			l, r = i, i
+			while r < n and string[r - l] == string[r]:
+				r += 1
+			z[i] = r - l
+			r -= 1
+		else:
+			k = i - l
+			if z[k] < r - i + 1:
+				z[i] = z[k]
+			else:
+				l = i
+				while r < n and string[r - l] == string[r]:
+					r += 1
+				z[i] = r - l
+				r -= 1
 
-find = "abc"
+def search(text, pattern):
 
-holestr = find + "$" + txt
+	# Create concatenated string "P$T"
+	concat = pattern + "$" + text
+	l = len(concat)
 
+	z = [0] * l
+	getZarr(concat, z)
 
+	for i in range(l):
+		if z[i] == len(pattern):
+			print("Pattern found at index",
+					i - len(pattern) - 1)
 
-for i in range(len(holestr)):
-    count = 0
-    if holestr[0]==holestr[len(find)+1]:
-        count+=1
-        i=0
-        while(holestr[i]==holestr[len(find)+1+i]):
-            print(holestr[i],holestr[len(find)+1+i])
-            i+=1
+text = "AAABC"
+pattern = "ABC"
+search(text, pattern)
