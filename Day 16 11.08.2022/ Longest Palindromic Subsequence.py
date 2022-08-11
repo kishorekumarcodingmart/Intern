d0c6e1854f890a20627511e5bbbe9020607d2baa
kishorekumarcodingmart <kishorekumar@codingmart.com>
2022-08-11 17:59:19 +0530

    DAy 16 Task

---
+++ Day 16 11.08.2022/ Longest Palindromic Subsequence.py
@@ -0,0 +1,24 @@
+text1 = "xyz"
+
+text2 = text1[::-1]
+
+
+dp = [[0 for j in range(len(text2)+1)] for i in range(len(text1)+1)]
+
+def findsol():
+
+    for i in range(1,len(text1)+1):
+        for j in range(1,len(text2)+1):
+
+            if text1[i-1]==text2[j-1]:
+                dp[i][j]=1+dp[i-1][j-1]
+            else:
+                dp[i][j]=max(dp[i-1][j], dp[i][j-1])
+
+
+    for i in dp:
+        print(i)
+    return dp[-1][-1]
+
+print(findsol())
+
